19b5dc9f0c597096e9114b7e3eaf3b976e1c2877
csnizik <csnizik@tulane.edu>
2025-06-28 15:50:38 -0500

    6

--- web/modules/custom/arsapps_module/src/Form/ArsappsModuleSettingsForm.php
+++ web/modules/custom/arsapps_module/src/Form/ArsappsModuleSettingsForm.php
@@ -80,14 +80,6 @@ final class ArsappsModuleSettingsForm extends ConfigFormBase {
 
   /**
    * {@inheritdoc}
-   *
-   * @param array<string, mixed> &$form
-   *   The form array.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The form state.
-   *
-   * @param-out array<string, mixed> $form
-   *   The form array, potentially modified by reference.
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
 
